5df1af3ca781d4e5624035ced36449d23d5d2d16
RohitBasantYadav <yadav.rohit.basant@gmail.com>
2024-06-19 20:01:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const btn = document.getElementById("btn");
+// btn.addEventListener("click", function(){
+//     window.location.href = "menu.html";
+// });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
